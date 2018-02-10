51d6e8e8a877d3f2272f8515c53cf67047cf545c
Cythral <talen@cythral.com>
2018-02-09 18:22:35 -0600

    Dynamically add unicode characters to css

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,29 @@ const
     path = require('path'),
     lessPluginCleanCSS = require('less-plugin-clean-css'),
     cleanCSSPlugin = new lessPluginCleanCSS({advanced: true}),
-    exec = require('child_process').execSync;
+    exec = require('child_process').execSync,
+    
+    lessSrcFile = "src/less/style.less";
     
 
 gulp.task('default', () => {
     exec("icon-font-generator src/svg/*.svg -o dist -c false -n ci");
     
-    less.render(fs.readFileSync("src/less/style.less").toString(), {
-        filename: path.resolve("src/less/style.less"),
+    let lessSrcString = fs.readFileSync(lessSrcFile).toString();
+    let extraLess = "";
+    let codes = JSON.parse(fs.readFileSync("dist/ci.json").toString());
+    
+    // add unicode characters to the css file
+    for(let code of Object.keys(codes)) {
+        extraLess += `&.ci-${code}:before { content: "${codes[code]}"; }`;
+    }
+
+    lessSrcString = lessSrcString.replace("{icons}", extraLess);
+
+    less.render(lessSrcString, {
+        filename: path.resolve(lessSrcFile),
         plugins: [cleanCSSPlugin]
 
-    }).then(output => {
-        var css = output.css;
-        var codes = JSON.parse(fs.readFileSync("dist/ci.json").toString());
+    }).then(output => fs.writeFileSync("dist/ci.css", output.css));
 
-        for(let code of Object.keys(codes)) {
-            css = css.replace(`{${code}}`, codes[code]);
-        }
-
-        fs.writeFileSync("dist/ci.css", css);
-        
-    });
 });
