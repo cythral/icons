95f49bec26197d6a3c9c9ead6baf853d5ebe0a12
Cythral <talen.fisher@cythral.com>
2018-07-06 17:01:01 -0500

    add website index

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,8 @@ const
     cleanCSSPlugin = new lessPluginCleanCSS({advanced: true}),
     exec = require('child_process').execSync,
     
-    lessSrcFile = "src/less/style.less";
+    lessSrcFile = "src/less/style.less",
+    lessPagesFile = "src/less/pages.less";
     
 
 gulp.task('default', () => {
@@ -32,3 +33,37 @@ gulp.task('default', () => {
     }).then(output => fs.writeFileSync("public/latest/ci.css", output.css));
 
 });
+
+gulp.task('pages.html', () => {
+    exec("mkdir -p public/img");
+    exec("cp -r src/img/* public/img");
+    
+    for(let filename of fs.readdirSync("src/html/pages/")) {
+        let file = fs.readFileSync("src/html/pages/"+filename).toString();
+        file = file.replace(/\{\{include:(.*?)\}\}/g, function(matches, n1) {
+            return fs.readFileSync("src/html/includes/"+n1+".html");
+        });
+
+        file = file.replace(/\{\{var:(.*?)\}\}/g, function(matches, n1) {
+            return {
+                title: filename.replace(".html", ""),
+                year: new Date().getFullYear()
+            }[n1] || "";
+        });
+
+        fs.writeFileSync("public/"+filename, file);
+    }
+});
+
+
+gulp.task('pages.css', () => {
+    exec("mkdir -p public/css");
+    let lessPagesString = fs.readFileSync(lessPagesFile).toString();
+
+    less.render(lessPagesString, {
+        filename: path.resolve(lessPagesFile),
+        plugins: [cleanCSSPlugin]
+    }).then(output => fs.writeFileSync("public/css/pages.css", output.css));
+});
+
+gulp.task("pages", [ "pages.html", "pages.css" ]);
